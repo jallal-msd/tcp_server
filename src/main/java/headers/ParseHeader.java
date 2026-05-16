37e509e10689127b15b0147aea603983d25faf56
jallal-msd <jallalblack@gmail.com>
2026-05-17 01:05:34 +0200

    test3of3

--- src/main/java/headers/ParseHeader.java
+++ src/main/java/headers/ParseHeader.java
@@ -1,37 +1,51 @@
 package headers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class ParseHeader {
 
-  public static String rn = "\r\n\r\n";
+  public static String rn = "\r\n";
 
-  public static Header parseHeader(String fieldLine) {
+  public static HashMap<String, Header> parseHeader(String fieldLine) {
+    System.out.println(fieldLine);
     Header header = new Header();
+    HashMap<String, Header> headerMap = new HashMap<>();
     List<String> parts = Arrays.asList(fieldLine.split(":", 2));
     if (parts.size() != 2) {
       System.err.println("malformed");
       return null;
     }
     System.out.println(parts.get(0));
+
+    String name = parts.get(0);
     header.setName(parts.get(0));
     header.setValue(parts.get(1).trim());
+    System.out.println("-- " + name);
+    headerMap.put(name, header);
+
     if (Character.isWhitespace(header.getName().charAt(header.getName().length() - 1))) {
       System.err.println("white space after name , malformed");
       return null;
     }
-    return header;
+    return headerMap;
 
   }
 
-  public static Header parse(byte[] buff) {
+  public static List<HashMap<String, Header>> parse(byte[] buff) {
     boolean done = false;
-    Header header = new Header();
+
+    int idx = 0;
+    HashMap<String, Header> header = new HashMap<>();
+    List<HashMap<String, Header>> listHeader = new ArrayList<>();
     int indx = 0;
     String strBuff = new String(buff);
     while (!done) {
-      indx = strBuff.indexOf(rn);
+      String chuckOfStr = strBuff.substring(idx, strBuff.length());
+      indx = chuckOfStr.indexOf(rn);
+      System.out.println(indx);
       if (indx == -1) {
         System.err.println("register nurse not found. malformed request");
       }
@@ -39,13 +53,14 @@ public class ParseHeader {
         System.out.println("found 0 ");
         // empty header, first index is \r\n
         done = true;
-        return null;
+        return listHeader;
+
       }
-      header = parseHeader(strBuff.substring(0, indx));
+      parseHeader(chuckOfStr.substring(0, indx));
+      idx += indx + rn.length();
+
       if (header != null) {
-        header.setLen(indx + rn.length());
       }
-      return header;
     }
     return null;
 
--- src/test/java/headers/ParseHeaderTest.java
+++ src/test/java/headers/ParseHeaderTest.java
@@ -3,6 +3,9 @@ package headers;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import headers.ParseHeader;
 import headers.Header;
@@ -11,25 +14,29 @@ public class ParseHeaderTest {
 
   @Test
   public void testParseHeader() {
-    String s = "Host: localhost:8000\r\n\r\n";
-    Header header = ParseHeader.parse(s.getBytes());
-    assertEquals("Host", header.getName());
-    assertEquals("localhost:8000", header.getValue());
-    assertEquals(24, header.getLen());
+    String s = "Host: localhost:8000\r\nFOOF: s\r\n\r\n";
+    List<HashMap<String, Header>> headers = ParseHeader.parse(s.getBytes());
+    for (HashMap<String, Header> header : headers) {
+
+      assertEquals("Host", header.get("Host").getName());
+      assertEquals("localhost:8000", header.get("Host").getValue());
+      assertEquals("FOOF", header.get("FOOF").getName());
+      assertEquals("s", header.get("FOOF").getValue());
+    }
 
   }
 
   @Test
   public void testEmptyParseHeader() {
     String s = "\r\n\r\n";
-    Header header = ParseHeader.parse(s.getBytes());
-    assertNull(header);
+    List<HashMap<String, Header>> headers = ParseHeader.parse(s.getBytes());
+    assertEquals(0, headers.size());
   }
 
   @Test
   public void testWsParseHeader() {
-    String st = "Host : localhost:8000\r\n\r\n";
-    Header header1 = ParseHeader.parse(st.getBytes());
-    assertNull(header1);
+    String s = "Host : localhost:8000\r\n\r\n";
+    List<HashMap<String, Header>> headers = ParseHeader.parse(s.getBytes());
+    assertEquals(0, headers.size());
   }
 }
